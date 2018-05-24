585cc595484868cd0e9ad3e634d93e60bf72cb60
CodeTeam <obacodeteam@gmail.com>
2018-05-24 19:32:23 +0200

    added pygame exampl 1

---
+++ Pygame voorbeeld 1/main.py
@@ -0,0 +1,36 @@
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+
+screen = pygame.display.set_mode((400, 300))
+
+pygame.display.set_caption("Hello OBA!")
+
+oba = pygame.image.load("logo oba.jpg")
+
+oba_x = 0
+oba_y = 0
+
+movespeed = 1
+
+while True: # main game loop
+	#input handling
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+		elif event.type == KEYDOWN:
+			if event.key == K_UP:
+				oba_y -= movespeed
+			elif event.key == K_DOWN:
+				oba_y += movespeed
+			elif event.key == K_LEFT:
+				oba_x -= movespeed
+			elif event.key == K_RIGHT:
+				oba_x += movespeed
+	
+	#drawing
+	screen.fill((0,0,0))
+	screen.blit(oba, (oba_x, oba_y))
+	pygame.display.update()
--- blankpygame.py
+++ blankpygame.py
@@ -5,7 +5,7 @@ pygame.init()
 DISPLAYSURF = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('Hello Pygame World!')
 while True: # main game loop
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			pygame.quit()
-			sys.exit()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
